8e1bf1c074e1f899e2531a3b221ad066145fcce5
Joy Renee Mugo <mugojoy24@gmail.com>
2024-04-11 16:55:06 +0300

    first commit

--- index.js
+++ index.js
@@ -11,6 +11,12 @@ const tutorials = [
   'what is JSONP?'
 ];
 
-const titleCased = () => {
-  return tutorials
+function titleCased(){
+  let newTutorials=tutorials.map(sentence=>{
+    let words = sentence.split(' ').map(word=>{
+      return word.charAt(0).toUpperCase()+ word.slice(1)
+    })
+    return words.join(' ')
+  });
+  return newTutorials;
 }
